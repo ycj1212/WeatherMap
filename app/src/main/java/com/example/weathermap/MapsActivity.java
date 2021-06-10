12a830afce40371d093dc33ef027153564e479be
Yang Cheol Joo <rlaengks0001@naver.com>
2021-06-11 00:38:32 +0900

    Add feature for request weather data

--- app/src/main/java/com/example/weathermap/MapsActivity.java
+++ app/src/main/java/com/example/weathermap/MapsActivity.java
@@ -5,9 +5,13 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,12 +19,24 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1001;
 
     private GoogleMap mMap;
+
+    private Document doc = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +46,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        GetXMLTask task = new GetXMLTask(this);
+        task.execute("https://www.kma.go.kr/wid/queryDFS.jsp?gridx=61&gridy=125");
     }
 
     @Override
@@ -39,9 +58,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapUiSetting.setZoomControlsEnabled(true);
         mapUiSetting.setCompassEnabled(true);
 
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(36, 127.8), 7));
 
         enableMyLocation();
     }
@@ -73,6 +90,59 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     android.os.Process.killProcess(android.os.Process.myPid());
                 }
                 return;
+        }
+    }
+
+    @SuppressLint("NewApi")
+    private class GetXMLTask extends AsyncTask<String, Void, Document> {
+        private Activity context;
+
+        public GetXMLTask(Activity context) {
+            this.context = context;
+        }
+
+        @Override
+        protected Document doInBackground(String... urls) {
+            URL url;
+
+            try {
+                url = new URL(urls[0]);
+                DocumentBuilderFactory dbf = DocumentBuilderFactory
+                        .newInstance();
+                DocumentBuilder db = dbf.newDocumentBuilder();
+
+                doc = db.parse(new InputSource(url.openStream()));
+                doc.getDocumentElement().normalize();
+            } catch (Exception e) {
+                Toast.makeText(getBaseContext(), "Parsing Error", Toast.LENGTH_SHORT).show();
+            }
+
+            return doc;
+        }
+
+        @Override
+        protected void onPostExecute(Document doc) {
+            String s = "";
+            NodeList nodeList = doc.getElementsByTagName("data");
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                s += "" + i + ": 날씨 정보: ";
+                Node node = nodeList.item(i);
+                Element fstElmnt = (Element) node;
+
+                NodeList nameList = fstElmnt.getElementsByTagName("temp");
+                Element nameElement = (Element) nameList.item(0);
+                nameList = nameElement.getChildNodes();
+                s += "온도 = " + ((Node) nameList.item(0)).getNodeValue() + " ,";
+
+                NodeList websiteList = fstElmnt.getElementsByTagName("wfKor");
+                Element websiteElement = (Element) websiteList.item(0);
+                websiteList = websiteElement.getChildNodes();
+                s += "날씨 = " + ((Node) websiteList.item(0)).getNodeValue()
+                        + "\n";
+            }
+
+            Toast.makeText(context, s, Toast.LENGTH_LONG).show();
         }
     }
 }
